08eac5b8c706541fa6837aff13cf2935b3ebf804
Michaud <michaud@venant.nl>
2021-10-11 16:29:38 +0200

    WIP clean-up

--- src/js/scene/addRendererSettings.ts
+++ src/js/scene/addRendererSettings.ts
@@ -8,6 +8,7 @@ import {
 const addRendererSettings = (renderer : WebGLRenderer) : void => {
 
     renderer.setPixelRatio(window.devicePixelRatio);
+
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = PCFSoftShadowMap;
     renderer.shadowMap.autoUpdate = true;
