9ca93640db1fba760e532662e9aadfdd38730e10
Michaud <michaud@venant.nl>
2021-10-11 16:27:36 +0200

    WIP set latest lighting

--- src/js/scene/addLighting.ts
+++ src/js/scene/addLighting.ts
@@ -1,4 +1,14 @@
-import { AmbientLight, DirectionalLight, HemisphereLight, IcosahedronGeometry, Mesh, MeshStandardMaterial, Scene, SpotLight, Vector3 } from "three";
+import {
+    AmbientLight,
+    DirectionalLight,
+    HemisphereLight,
+    IcosahedronGeometry,
+    Mesh,
+    MeshStandardMaterial,
+    Scene,
+    SpotLight,
+    Vector3
+} from "three";
 
 const addLighting = (scene : Scene) : void => {
 
@@ -10,9 +20,10 @@ const addLighting = (scene : Scene) : void => {
     fillLight.position = fillLightPos;
     scene.add( fillLight );
 
-    const fillLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
-    fillLightPositionMesh.position.set(fillLightPos.x, fillLightPos.y, fillLightPos.z);
-    scene.add(fillLightPositionMesh);
+    /* keep for debugging */
+    // const fillLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
+    // fillLightPositionMesh.position.set(fillLightPos.x, fillLightPos.y, fillLightPos.z);
+    // scene.add(fillLightPositionMesh);
 
     const highLightPos = new Vector3(-1, .25, 0.5);
     const highLight = new DirectionalLight( 0xccccff, 3 );
@@ -20,59 +31,26 @@ const addLighting = (scene : Scene) : void => {
     highLight.position = highLightPos;
     scene.add( highLight );
 
-    const highLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
-    highLightPositionMesh.position.set(highLightPos.x, highLightPos.y, highLightPos.z);
-    scene.add(highLightPositionMesh);
+    /* keep for debugging */
+    // const highLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
+    // highLightPositionMesh.position.set(highLightPos.x, highLightPos.y, highLightPos.z);
+    // scene.add(highLightPositionMesh);
 
-    const topLightPos = new Vector3(-.15, .45, 0.75);
-    const topLight = new DirectionalLight( 0xccccff, 8 );
+    const topLightPos = new Vector3(-.15, -.15, 1.15);
+    const topLight = new DirectionalLight( 0xccccff, 10 );
     topLightPos.setLength(posScalar);
     topLight.position = topLightPos;
     scene.add( topLight );
 
-    const topLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
-    topLightPositionMesh.position.set(topLightPos.x, topLightPos.y, topLightPos.z);
-    scene.add(topLightPositionMesh);
+    /* keep for debugging */
+    // const topLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 4));
+    // topLightPositionMesh.position.set(topLightPos.x, topLightPos.y, topLightPos.z);
+    // scene.add(topLightPositionMesh);
 
-    // /* Add lights */
-    // const spotLight: SpotLight = new SpotLight(0xffffff, 1);
-    // spotLight.position.set(-200, 400, 400);
-
-    // // Create a red material for the nose.
-    // const spotLightPositionMaterial = new MeshStandardMaterial({
-    //     color: 0xff0000,
-    //     roughness: 0.4,
-    //     metalness: 0.1,
-    //     transparent: true,
-    // });
-
-    // const spotLightPositionMesh = new Mesh(new IcosahedronGeometry(5, 3), spotLightPositionMaterial);
-    // spotLightPositionMesh.position.set(0, 0, 100);
-    // scene.add(spotLightPositionMesh);
-
-
-    // scene.add(spotLight);
-
-    // spotLight.castShadow = true;
-
-    // spotLight.shadow.mapSize.width = 1024;
-    // spotLight.shadow.mapSize.height = 1024;
-
-    // spotLight.shadow.camera.near = 200;
-    // spotLight.shadow.camera.far = 800;
-
-    // spotLight.shadow.camera.fov = 40;
-
-    // spotLight.shadow.bias = -0.001125;
-
-    // scene.add(spotLight);
-
-    const hemiLight: HemisphereLight = new HemisphereLight(0xffffff, 0x080808, .5);
+    const hemiLight: HemisphereLight = new HemisphereLight(0xffffff, 0x080808, .75);
     scene.add(hemiLight);
 
-    // const ambientLight : AmbientLight = new AmbientLight(0x001a24, .51);
-    // scene.add(ambientLight);
-    const ambientLight2 : AmbientLight = new AmbientLight(0xffffff, .85);
+    const ambientLight2 : AmbientLight = new AmbientLight(0xffffff, 1.15);
     scene.add(ambientLight2);
 }
 
