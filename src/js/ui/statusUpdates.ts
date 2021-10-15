bce82a09f4e2b90ca350c9a4eeb6c99e3e663770
Michaud <michaud@venant.nl>
2021-10-15 17:51:49 +0200

    FRMT code

--- src/js/ui/statusUpdates.ts
+++ src/js/ui/statusUpdates.ts
@@ -1,13 +1,13 @@
-export const onClear = (status:any) => () => {
+export const onClear = (status : any) => () => {
     status.value = "loaded";
     status.hidden = true;
 };
 
-export const onLoadModel = (status:any) => () => {
+export const onLoadModel = (status : any) => () => {
     status.value = "Loading model...";
 };
 
-export const onFirstFaceDetection = (status:any) => () => {
+export const onFirstFaceDetection = (status : any) => () => {
 
     status.value = "Detecting face...";
 };
