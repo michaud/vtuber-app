721a89996e99b3586814981451d5e000b32e580d
Michaud <michaud@venant.nl>
2021-10-21 18:28:24 +0200

    DET add better lighting

--- src/js/scene/addLighting.ts
+++ src/js/scene/addLighting.ts
@@ -14,15 +14,14 @@ import {
 
 const addLighting = (scene : Scene) : Array<Light> => {
 
-    const fillLightPos = new Vector3(400, -140, 0);
-    const fillLight = new SpotLight( 0xffddcc, 0, 10);
+    const fillLightPos = new Vector3(500, -400, 0);
+    const fillLight = new SpotLight( 0xffddcc, 300);
     fillLight.position.copy(fillLightPos);
     fillLight.name = 'fillLight';
     scene.add( fillLight );
 
-    const highLightPos = new Vector3(-400, 100, 200);
-    const highLight = new SpotLight( 0xccccff, .2, 150);
-    highLight.power = 1;
+    const highLightPos = new Vector3(-400, 400, 200);
+    const highLight = new SpotLight( 0xffffff, 1400);
     highLight.name = 'highLight';
     highLight.position.copy(highLightPos);
     scene.add( highLight );
@@ -34,12 +33,12 @@ const addLighting = (scene : Scene) : Array<Light> => {
     topLight.position.copy(topLightPos);
     scene.add( topLight );
 
-    const hemiLight: HemisphereLight = new HemisphereLight(0xffffff, 0x080808, 0);
+    const hemiLight: HemisphereLight = new HemisphereLight(0xffffff, 0x080808, .75);
     hemiLight.name = 'hemiLight';
 
     scene.add(hemiLight);
 
-    const ambientLight2 : AmbientLight = new AmbientLight(0xffffff, 0);
+    const ambientLight2 : AmbientLight = new AmbientLight(0xffffff, 0.25);
     ambientLight2.name = 'ambientLight2';
 
     scene.add(ambientLight2);
