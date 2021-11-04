f4d82c99bab1c47b8cb95177df5a81619b32fd0d
Michaud <michaud@venant.nl>
2021-11-04 09:56:36 +0100

    CLN

--- src/js/scene/createScene.ts
+++ src/js/scene/createScene.ts
@@ -7,12 +7,10 @@ import {
 } from 'three';
 
 import { SceneResources } from 'types/SceneResources';
-/* import addHDR from './addHDR'; */
 import addRendererSettings from './addRendererSettings';
 
 const createSene = (canvas : HTMLCanvasElement): SceneResources => {
 
-    /* Set a background color, or change alpha to false for a solid canvas. */
     const renderer: WebGLRenderer = new WebGLRenderer({
         antialias: true,
         alpha: true,
@@ -22,18 +20,12 @@ const createSene = (canvas : HTMLCanvasElement): SceneResources => {
     addRendererSettings(renderer);
 
     const scene: Scene = new Scene();
+
     const camera: Camera = new OrthographicCamera(
         1, 1, 1, 1,
         -10000, 10000
     );
-    // const camera: Camera = new PerspectiveCamera(
-    //     50,
-    //     1,
-    //     0.1,
-    //     10000
-    // );
     camera.position.z = 4000;
-    //addHDR(scene, renderer);
 
     return {
         renderer,
